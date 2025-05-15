0ced58aea56ac5d6584db58680e4d63002f9290d
davarng <113583854+davarng@users.noreply.github.com>
2025-05-15 11:16:33 +0200

    first push for assignment

---
+++ EmployeesApp.Web/Controllers/EmployeesController.cs
@@ -0,0 +1,59 @@
+﻿using EmployeesApp.Web.Models;
+using EmployeesApp.Web.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EmployeesApp.Web.Controllers;
+
+public class EmployeesController : Controller
+{
+    static EmployeeService employeeService = new EmployeeService();
+
+    public EmployeesController() { }
+
+    [HttpGet("")]
+    public IActionResult Index()
+    {
+        var model = employeeService.GetAllEmployees();
+        return View(model);
+    }
+
+    //[HttpPost("{id}")]
+    //public IActionResult Delete(int id)
+    //{
+    //    employeeService.DeleteEmployee(id);
+    //    return RedirectToAction(nameof(Index));
+    //}
+
+    [HttpGet("create")]
+    public IActionResult Create()
+    {
+        return View();
+    }
+
+    [HttpPost("create")]
+    public IActionResult Create(Employee employee)
+    {
+        employeeService.Add(employee);
+
+        return RedirectToAction(nameof(Index));
+    }
+
+    //[HttpPost("edit")]
+    //public IActionResult Edit(int id, string name, int age)
+    //{
+
+    //    employeeService.EditEmployee(id, name, age);
+
+    //    return RedirectToAction(nameof(Index));
+    //}
+
+    //[HttpGet("edit/{id}")]
+    //public IActionResult Edit(int id)
+    //{
+    //    var model = employeeService.GetEmployeeById(id);
+    //    return View(model);
+    //}
+
+
+}
+
---
+++ EmployeesApp.Web/Services/EmployeeService.cs
@@ -0,0 +1,25 @@
+﻿using EmployeesApp.Web.Models;
+
+namespace EmployeesApp.Web.Services
+{
+    public class EmployeeService
+    {
+        private List<Employee> employees = [new Employee { Id = 10, Name = "Niklas", Email = "Test" }];
+
+        public void Add(Employee employee)
+        {
+            employees.Add(employee);
+        }
+
+        public Employee GetEmployeeById(int id)
+        {
+            return employees.SingleOrDefault(e => e.Id == id);
+        }
+
+        public Employee[] GetAllEmployees()
+        {
+            return employees.ToArray();
+        }
+
+    }
+}
